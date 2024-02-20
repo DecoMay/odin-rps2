fe64275cc13f1af7fbec701dd812c9b45c641951
DecoMay <143063276+DecoMay@users.noreply.github.com>
2024-02-20 18:04:58 +0800

    Add playRound function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 function getComputerChoice() {
     let choice = '';
     let randomNumber = Math.floor(Math.random() * (3 - 1 + 1) + 1);
-    console.log(randomNumber);
     if (randomNumber === 1) {
         choice = 'Rock';
     } else if (randomNumber === 2) {
@@ -13,4 +12,19 @@ function getComputerChoice() {
     return choice;
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let gameResult = '';
+    if ((playerSelection == 'rock' && computerSelection == 'Paper') || (playerSelection == 'paper' && computerSelection == 'Scissor') || (playerSelection == 'scissor' && computerSelection == 'Rock')) {
+        gameResult = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if ((playerSelection == 'rock' && computerSelection == 'Scissor') || (playerSelection == 'paper' && computerSelection == 'Rock') || (playerSelection == 'scissor' && computerSelection == 'Paper')) {
+        gameResult = `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        gameResult = 'Draw';
+    }
+    return gameResult;
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
